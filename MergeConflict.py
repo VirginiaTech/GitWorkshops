120fdf22a90db68e27baf9c895fde91e0f9d173e
Helen Williams <helen.williams@made.com>
2022-03-25 09:01:13 +0000

    linting and file renaming

--- MergeConflict.py
+++
@@ -1,7 +0,0 @@
-# Welcome to the GitHub Workshop!
-# We'll explore one of the most dreaded - but inevitable - things in Git... Merge Conflicts!
-
-# Each person should change to following line of code:
-
-print("This was initially just a simple sentence, but now I made it look like code. It doesn't actually matter, but it'll look more familiar this way");
-print("Hello world, my name is <NAME_HERE> and my favorite programming language is <LANGUAGE_HERE> ");
---
+++ merge_conflict.py
@@ -0,0 +1,8 @@
+# Welcome to the GitHub Workshop!
+# We'll explore one of the most dreaded - but inevitable - things in Git... Merge Conflicts!
+
+# Each person should change to following line of code:
+
+print(
+    "Hello world, my name is <NAME_HERE> and my favourite drink is <DRINK_HERE> "
+)
