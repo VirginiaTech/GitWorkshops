20218403e5fc601e7fa3c722911b678d74f6bde8
Pro4TLZZ <github@pro4tlzz.com>
2022-03-25 11:01:12 +0000

    change merge conflict

--- merge_conflict.py
+++ merge_conflict.py
@@ -4,5 +4,5 @@
 # Each person should change to following line of code:
 
 print(
-    "Hello world, my name is <NAME_HERE> and my favourite drink is <DRINK_HERE> "
+    "Hello world, my name is Bilal and my favourite drink is Irn Bru "
 )
